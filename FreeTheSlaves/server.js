88496747f717975f75bf00bcad486424dbf0536e
Henry Thomas <hthomas@stevens.edu>
2016-10-28 23:52:06 -0400

    figured out what heck the middleware was doin

--- FreeTheSlaves/server.js
+++ FreeTheSlaves/server.js
@@ -1,20 +1,29 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const configureRoutes = require("./mongoDB/routes");
+  // We first require our express package
+var express = require('express');
+var bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-configureRoutes(app);
+// This package exports the function to create an express instance:
+var app = express();
 
-//app.use("/", express.static('www'));
+// This is called 'adding middleware', or things that will help parse your request
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get("/", function (request, response) {
-    console.log("Hello!!");
-    response.sendFile("./index.html", {root: __dirname });
+// This middleware will activate for every request we make to 
+// any path starting with /assets;
+// it will check the 'static' folder for matching files 
+//app.use('/assets', express.static('www'));
+
+// Setup your routes here!
+
+app.get("/", function (request, response) { 
+    // We have to pass a second parameter to specify the root directory
+    // __dirname is a global variable representing the file directory you are currently in
+    response.sendFile("./www/index.html", { root: __dirname });
 });
 
-app.listen(3000, () => {
-    console.log("We got a a server!");
-    console.log("Your routes will be running on http://localhost:3000");
+
+// We can now navigate to localhost:3000
+app.listen(3000, function () {
+    console.log('Your server is now listening on port 3000! Navigate to http://localhost:3000 to access it');
 });
-    

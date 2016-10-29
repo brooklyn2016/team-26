e825f4429c620aac1c3e596c44bae74ca2f30473
Jason Sarwar <jsarwar@stevens.edu>
2016-10-29 03:38:26 -0400

    Jason: Got rid of Routes folder #EpicFail #RoundHouseCommit #ChuckNorris

--- FreeTheSlaves/mongoDB/data/users.js
+++ FreeTheSlaves/mongoDB/data/users.js
@@ -9,3 +9,40 @@ const mongoCollections = require("../config/mongoCollections");
 
 const users = mongoCollections.Users;
 
+const uuid = require('node-uuid');
+
+let exportedMethods = {
+    
+    addUser(userName, passWord) {
+    
+        var newUser = { 
+           _id: uuid.v4()
+        };
+  
+        
+        if(userName !== undefined) {
+            
+            /* Check for duplicates! */
+            
+            newUser.userName = userName;   
+        } else {
+            return Promise.reject("Username not given");
+        }
+        
+        if(passWord !== undefined) {
+            newUser.passWord = passWord;   
+        } else {
+            return Promise.reject("Username not given");
+        }
+    
+    
+    },
+    
+    authenticateUser(userName, passWord) {
+    
+    
+    },
+    
+}
+
+module.exports = exportedMethods;
